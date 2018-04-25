d86e313cee264b82c6307165fda73c05c0052e03
Ruwen Moos <moos@fzi.de>
2018-04-25 09:37:26 +0200

    Bootstrap

---
+++ vislab-category-core/src/main/java/de/hska/vislab/micro/CategoryCoreApplication.java
@@ -0,0 +1,12 @@
+package de.hska.vislab.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryCoreApplication.class, args);
+	}
+}
